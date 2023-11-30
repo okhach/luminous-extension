7a628d63684d49d56683f679566959ee430d50e1
steveniscoding <chun.hu@student.unimelb.edu.au>
2023-11-30 15:36:42 +1100

    modify captureVisibleTab

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('captureBtn').addEventListener('click', function() {
-      chrome.tabs.captureVisibleTab(null, { format: 'png' }, function(dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
+      chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (dataUrl) {
+        // Convert Data URL to Blob
+        var blob = dataURLToBlob(dataUrl);
+
+        // Send the blob to your server
+        uploadToServer(blob, 'screenshot.png');
       });
     });
-  });
+});
+
   
