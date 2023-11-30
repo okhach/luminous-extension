c7f06b9b3fefcaace196d77f25621cc43db1c033
steveniscoding <chun.hu@student.unimelb.edu.au>
2023-11-30 15:37:28 +1100

    add dataURLToBlob function

--- popup.js
+++ popup.js
@@ -10,4 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-  
+function dataURLToBlob(dataUrl) {
+  var byteString = atob(dataUrl.split(',')[1]);
+  var mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0];
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeString });
+}
